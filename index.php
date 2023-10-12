5b00361799bd35f2ac3b3a64d24d459f41c58f3a
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2023-10-12 16:15:07 +0000

    Pagina iniziale

--- index.php
+++ index.php
@@ -1,10 +1,87 @@
 <!DOCTYPE html>
+
+<?php
+function getDirContents($dir, &$results = array(), $bpath = "")
+{
+    if (is_dir($dir)) {
+
+        $files = scandir($dir);
+
+        foreach ($files as $key => $value) {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+            if ($value != "." && $value != ".." &&  $value[0] != '.') {
+                if (is_dir($path)) {
+                    $results[] = $bpath . '/' . $value . '/';
+                    getDirContents($path, $results, $bpath . '/' . $value);
+                }
+            }
+        }
+    }
+    return $results;
+}
+?>
+
 <html>
-    <body>
-        <a href="/dispensa/">Visualizza dispensa</a>
-        <br>
-        <a href="/esempio-html/">Visualizza Esempio HTML</a>
-        <br>
-        <a href="/curriculim"> curriculim </a>
-    </body>
+
+<body>
+    <div class="blocco-link teoria">
+
+        <h1>Teoria offerta dal prof Longhin</h1>
+        <?php
+        foreach (getDirContents(__DIR__ . '/teoria') as $folder) {
+            echo "<a href='$folder'> $folder </a> <br>";
+        }
+
+        ?>
+    </div>
+
+    <div class="blocco-link esercizi">
+
+        <h1>Esercizi fatti da Me</h1>
+        <?php
+        foreach (getDirContents(__DIR__ . '/esercizi') as $folder) {
+            echo "<a href='$folder'> $folder </a> <br>";
+        }
+
+        ?>
+    </div>
+
+
+</body>
+
 </html>
+
+
+<style>
+    h1 {
+        text-align: center;
+    }
+
+    .blocco-link {
+        border-radius: 50px;
+        padding: 50px 50px;
+        margin-bottom: 20px;
+    }
+
+    .blocco-link {
+        width: 40%;
+    }
+
+    .teoria {
+        background-color: aqua;
+    }
+
+    .esercizi {
+        background-color: plum;
+    }
+
+    .blocco-link a {
+        font-size: 20px;
+    }
+
+    @media screen and (max-width: 700px) {
+        .blocco-link {
+            width: 100%;
+        }
+    }
+</style>
